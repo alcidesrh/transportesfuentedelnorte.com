85b1e61b263798028bd03b0032c08839326e186a
root <alcidesrh@gmail.com>
2022-11-19 06:29:31 -0600

    Country, states, cities.

--- src/Form/ClienteReservacionType.php
+++ src/Form/ClienteReservacionType.php
@@ -4,12 +4,9 @@ namespace App\Form;
 
 use App\Entity\ClienteReservacion;
 use App\Entity\Reservacion;
-use App\Entity\Tarjeta;
 use App\Repository\TarjetaRepository;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
